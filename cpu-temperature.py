aabcd27d9d1d30d9461f5d4697499ffa1e2eb607
FelitscheD <76789330+FelitscheD@users.noreply.github.com>
2021-03-27 23:21:58 +0100

    Update cpu-temperature.py

--- cpu-temperature.py
+++ cpu-temperature.py
@@ -1,11 +1,30 @@
+#Erstellt von FelitscheD
+#Version 1.0
+#Liest die CPU des Raspberry Pi 4 aus
+#Streamt diese an die API eines Livedatasets von Power BI
+ 
+import json
+import traceback
+import urllib
+from datetime import datetime
+import urllib.request
+
+header = {"Content-Type": "application/json; charset=utf-8"}
+url = "***Url-API***"
+
+while True:
+ try:
+#CPU-Temperatur auslesen und schreiben
   tempData = "/sys/class/thermal/thermal_zone0/temp"
   dateilesen = open(tempData, "r")
   temperature = dateilesen.readline(2)
   dateilesen.close()
   print(temperature)
+#Zeitstempel erstellen und schreiben
   now = datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S%Z")
   timestamp = now
   print(timestamp)
+#JSON für Power BI API erstellen
   formdata = {
       "temperature": (temperature),
       "timestamp": (timestamp)
